f58b439526cfede92bf9032a3c0e04e555817bf9
PowerfulTR <mintch2001@gmail.com>
2020-05-22 22:44:22 +0700

    Project 3

---
+++ SessionLogin/SessionLogin/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SessionLogin.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server"> ID : </asp:Label>
+            <asp:TextBox runat="server" ID="txtID"></asp:TextBox><br />
+            <asp:Label runat="server"> Password : </asp:Label>
+            <asp:TextBox runat="server" ID="txtPassword"></asp:TextBox>
+            <asp:button runat="server" ID="loginClick" Text="Login" OnClick="loginClick_Click"/>
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ SessionLogin/SessionLogin/index.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionLogin
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        protected void loginClick_Click(object sender, EventArgs e)
+        {
+            WriteID(txtID.Text);
+            WritePass(txtPassword.Text);
+            Check();
+        }
+        private void Check()
+        {
+            if (ReadID() == "" || ReadPass() == "")
+            {
+                
+            }
+            else
+            {
+                Server.Transfer("Home.aspx");
+            }
+        }
+        protected void WriteID(string id)
+        {
+            Session["userID"] = id;
+        }
+        protected void WritePass(string pass)
+        {
+            Session["userPass"] = pass;
+        }
+        private string ReadID()
+        {
+            if (Session["userID"] == null)
+            {
+                return "";
+            }
+            else
+            {
+                return Session["userID"].ToString();
+            }          
+        }
+        private string ReadPass()
+        {
+            if (Session["userPass"] == null)
+            {
+                return "";
+            }
+            else
+            {
+                return Session["userPass"].ToString();
+            }          
+        }
+    }
+}
